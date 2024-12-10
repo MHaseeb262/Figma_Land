9f346b4133217bedaa4f3399d2ac97866b7d3403
Muhammad Haseeb <haseebdeewana86@gmail.com>
2024-12-10 12:39:52 -0800

    I'm update the JS code for only mobile screen size and contect section alignment...

--- main.js
+++ main.js
@@ -1,7 +1,9 @@
 const paragraph = document.getElementsByClassName('rewrite');
 const para_len = paragraph.length;
 
-for (let i = 0; i < para_len; i++) {
-    paragraph[i].textContent = "Most calendars are designed for teams.";
+if (window.innerWidth <= 599) {
+    for (let i = 0; i < para_len; i++) {
+        paragraph[i].textContent = "Most calendars are designed for teams.";
 
+    }
 }
