af0849222f7dfc73b450657055aed24db4f60a85
Muhammad Haseeb <haseebdeewana86@gmail.com>
2024-12-11 16:59:08 -0800

    I give a path on navigation link in nav bar and in feature section I add video...

--- main.js
+++ main.js
@@ -7,3 +7,44 @@ if (window.innerWidth <= 599) {
 
     }
 }
+
+let video = document.getElementById('myVideo');
+let btn = document.getElementById('play_btn');
+let btnIcon = document.getElementById('btnIcon');
+let timeoutId;
+let bool = 0;
+
+btn.addEventListener('click', () => {
+    if (video.paused) {
+        video.play();
+        video.setAttribute("controls", "");
+        btnIcon.src = "/Images/pause_btn.svg";
+        hide_btn();
+
+        // hovering on video     
+        video.addEventListener('mouseenter', () => {
+            show_btn();
+            hide_btn();
+        });
+
+        video.addEventListener('mouseleave', () => {
+            hide_btn();
+        });
+    } else {
+        video.pause();
+        video.removeAttribute('controls');
+        btnIcon.src = "/Images/play_btn.svg";
+    }
+});
+
+
+
+function show_btn() {
+    btn.classList.remove('hidden');
+}
+
+function hide_btn() {
+    setTimeout(() => {
+        btn.classList.add('hidden');
+    }, 2500);
+}
